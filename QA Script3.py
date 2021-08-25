00fdfba1f77c1188ce2bbfec602ce555f972972e
nubletslol <nubletslol@gmail.com>
2021-08-25 01:41:42 -0400

    Update QA Script3.py

--- QA Script3.py
+++ QA Script3.py
@@ -7,30 +7,30 @@ from bs4 import BeautifulSoup
 import re
 
 driver = webdriver.FirefoxProfile()
-#downloads the website html file so that we are able to read the source code per line, this would allow for us to check the payeeID to see if the url is valid, then records it
+#downloads the website html file so that we are able to read the source code per line, this would allow for us to check the payorID to see if the url is valid, then records it
 urltester = ("https://www.dentrix.com/products/eservices/eclaims/payor-search?start=0&keyword=")
 pull = requests.get(urltester)
 print(pull.text)
 with open ("QA Test file.txt", 'w') as f:
     f.write(pull)
 
-payeeID = []
+payorID = []
 with open("QA Test file.txt") as f:
     while ( line := f.readLine().rstrip()):
         if ('/products/eservices/eclaims/payor-search?start=0&keyword=' in line):
-            payee = payeeID.append(re.search('*">(.*)</a></td*', line)) #parses and appends payeeID to a list
+            payor = payorID.append(re.search('*">(.*)</a></td*', line)) #parses and appends payorID to a list
 
-#script enters the payeeid through the search bar to see if its valid
-for i in payeeID:
-    urltester = ("https://www.dentrix.com/products/eservices/eclaims/payor-search?start=0&keyword="+payeeID[i])
+#script enters the payorid through the search bar to see if its valid
+for i in payorID:
+    urltester = ("https://www.dentrix.com/products/eservices/eclaims/payor-search?start=0&keyword="+payorID[i])
     pull = requests.get(urltester)
     print(pull.text)
 
     driver.get("https://www.dentrix.com/products/eservices/eclaims/payor-search?start=0&keyword=")
     searchbar= driver.find_element_by_css_selector('keyword')
-    searchbar.send_keys(payeeID[i])
+    searchbar.send_keys(payorID[i])
     page = driver.page_source
     if pull.text != page :
-        print("Issue found with payeeID: " + payeeID[i])
+        print("Issue found with payorID: " + payorID[i])
         i = 700
     #tests the page with the auto entered one to see if it works and is reliable

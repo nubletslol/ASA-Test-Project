9ae9399e6f48407536ea436043662eb4993d99aa
nubletslol <nubletslol@gmail.com>
2021-08-25 01:42:34 -0400

    Update QA script1.py

--- QA script1.py
+++ QA script1.py
@@ -4,9 +4,9 @@ import random
 import string
 import urllib3
 from bs4 import BeautifulSoup
-# makes a random value the payeeID and searches through it to see if it is valid
+# makes a random value the payorID and searches through it to see if it is valid
 # script then would use the webdriver to enter the value manually to the same link
-# afterwords it would compare the html files if the values are the same. If it isnt then it would spit out a error with the payeeID and would stop the run
+# afterwords it would compare the html files if the values are the same. If it isnt then it would spit out a error with the payorID and would stop the run
 
 driver = webdriver.FirefoxProfile()
 
@@ -23,7 +23,7 @@ for i in range (0,600):
     searchbar.send_keys(randomnumber)
     page = driver.page_source
     if pull.text != page :
-        print("Issue found with payeeID: " + randomnumber)
+        print("Issue found with payorID: " + randomnumber)
         i = 700
     
     

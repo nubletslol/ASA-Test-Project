7afbaf13ece935a4e48dc19b5390fce508928561
nubletslol <nubletslol@gmail.com>
2021-08-26 21:43:23 -0400

    Update QA Script3.py

--- QA Script3.py
+++ QA Script3.py
@@ -1,4 +1,3 @@
-from typing import Text
 import requests
 from selenium import webdriver
 import random
@@ -9,34 +8,33 @@ import re
 
 Driver_path = 'C:/Users/Khoa Ng/Documents/GitHub/chromedriver'
 driver = webdriver.Chrome(executable_path=Driver_path)
-#downloads the website html file so that we are able to read the source code per line, this would allow for us to check the payor name to see if the url is valid, then records it
+#downloads the website html file so that we are able to read the source code per line, this would allow for us to check the payeeID to see if the url is valid, then records it
 urltester = ("https://www.dentrix.com/products/eservices/eclaims/payor-search?start=0&keyword=")
 pull = requests.get(urltester)
 print(pull.text)
 with open ("QA Test file.txt", 'w') as f:
     f.write(pull.text)
 
-payorName = []
+payeeID = []
 with open("QA Test file.txt") as f:
     while ( line := f.readline().rstrip()):
-        if ('//products//eservices//eclaims//payor-search?start=0&keyword=' in line):
-            payor = payorName.append(re.search('*<//td><td>(.*)<//td><td*>', line)) #parses and appends payor name to a list
-            print(payor)
-#script enters the payor name through the search bar to see if its valid
-for i in payorName:
-    urltester = ("https://www.dentrix.com/products/eservices/eclaims/payor-search?start=0&keyword="+payorName[i])
+        if ('/products/eservices/eclaims/payor-search?start=0&keyword=' in line):
+            payee = payeeID.append(re.search('*">(.*)</a></td*', line)) #parses and appends payeeID to a list
+
+#script enters the payeeid through the search bar to see if its valid
+for i in payeeID:
+    urltester = ("https://www.dentrix.com/products/eservices/eclaims/payor-search?start=0&keyword="+payeeID[i])
     pull = requests.get(urltester)
-    
+    print(pull.text)
 
     driver.get("https://www.dentrix.com/products/eservices/eclaims/payor-search?start=0&keyword=")
     searchbar= driver.find_element_by_css_selector('keyword')
-    searchbar.send_keys(payorName[i])
+    searchbar.send_keys(payeeID[i])
     page = driver.page_source
     if pull.text != page :
-        print("Issue found with Payor Name: " + payorName[i])
-        break
+        print("Issue found with payeeID: " + payeeID[i])
+        i = 700
     #tests the page with the auto entered one to see if it works and is reliable
-
 
 
 
